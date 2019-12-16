3066a9eb7e848ea6a752547cb6c36f8b409f076a
Iván Herranz <ihcuesta@gmail.com>
2019-12-16 02:06:06 +0100

    new battle features

--- js/pikachu.js
+++ js/pikachu.js
@@ -2,28 +2,35 @@ class Pikachu {
     constructor () {
         this.name = "Pikachu",
         this.life = 100,
-        this.attacks = [
-            [
-                {attack: "Usar funciones", goodPractice: true},
-                {attack: "Repetir código", goodPractice: false}
-            ],
-            [
-                {attack: "Hoja CSS", goodPractice: true},
-                {attack: "Estilos en línea", goodPractice: false}
-            ],
-            [
-                {attack: "Crear varias ramas", goodPractice: true},
-                {attack: "Usar solo la rama master", goodPractice: false}
-            ],
-            [
-                {attack: "Comentar el código", goodPractice: true},
-                {attack: "No comentar el código", goodPractice: false}
-            ]
-        ]
+        this.lifeBar = 300,
+        this.damage = 25,
+        this.damageBar = 300 * this.damage / 100
+
+        // this.attacks = [
+        //     [
+        //         {attack: "Usar funciones", goodPractice: true},
+        //         {attack: "Repetir código", goodPractice: false}
+        //     ],
+        //     [
+        //         {attack: "Hoja CSS", goodPractice: true},
+        //         {attack: "Estilos en línea", goodPractice: false}
+        //     ],
+        //     [
+        //         {attack: "Crear varias ramas", goodPractice: true},
+        //         {attack: "Usar solo la rama master", goodPractice: false}
+        //     ],
+        //     [
+        //         {attack: "Comentar el código", goodPractice: true},
+        //         {attack: "No comentar el código", goodPractice: false}
+        //     ]
+        // ]
     }
 
-    attack() {
-        
+    receiveDamage() {
+        this.life -= this.damage;
+        this.lifeBar -= this.damageBar;
+        if (this.life < 0) this.life = 0;
+        if (this.lifeBar < 0) this.lifeBar = 0;
     }
 
 
